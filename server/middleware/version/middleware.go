74a562cd54a098805dcb50dc00f3bfe77b4e8666
Tim Schindler <xh3b4sd@gmail.com>
2016-12-10 01:28:41 +0100

    added server package

---
+++ server/middleware/version/middleware.go
@@ -0,0 +1,58 @@
+package version
+
+import (
+	micrologger "github.com/giantswarm/microkit/logger"
+	kitendpoint "github.com/go-kit/kit/endpoint"
+	"golang.org/x/net/context"
+
+	"github.com/giantswarm/microkit-example/service"
+)
+
+// Config represents the configuration used to create a version middleware.
+type Config struct {
+	// Dependencies.
+	Logger  micrologger.Logger
+	Service *service.Service
+}
+
+// DefaultConfig provides a default configuration to create a new version
+// middleware by best effort.
+func DefaultConfig() Config {
+	return Config{
+		// Dependencies.
+		Logger:  nil,
+		Service: nil,
+	}
+}
+
+// New creates a new configured version middleware.
+func New(config Config) (*Middleware, error) {
+	// Dependencies.
+	if config.Logger == nil {
+		return nil, maskAnyf(invalidConfigError, "logger must not be empty")
+	}
+	if config.Service == nil {
+		return nil, maskAnyf(invalidConfigError, "service must not be empty")
+	}
+
+	newMiddleware := &Middleware{
+		Config: config,
+	}
+
+	return newMiddleware, nil
+}
+
+type Middleware struct {
+	Config
+}
+
+func (m *Middleware) Middleware(next kitendpoint.Endpoint) kitendpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		m.Logger.Log(
+			"msg", "version middleware was called",
+			"fixme", "I do useless shit",
+		)
+
+		return next(ctx, request)
+	}
+}
