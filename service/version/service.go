99099d5b5d93efa2a5465c105f0403751bea9c14
Tim Schindler <xh3b4sd@gmail.com>
2016-12-12 11:59:24 +0100

    added service package (#1)

---
+++ service/version/service.go
@@ -0,0 +1,92 @@
+package version
+
+import (
+	"runtime"
+
+	micrologger "github.com/giantswarm/microkit/logger"
+)
+
+// Config represents the configuration used to create a version service.
+type Config struct {
+	// Dependencies.
+	Logger micrologger.Logger
+
+	// Settings.
+	Description    string
+	GitCommit      string
+	Name           string
+	ProjectVersion string
+	Source         string
+}
+
+// DefaultConfig provides a default configuration to create a new version service
+// by best effort.
+func DefaultConfig() Config {
+	return Config{
+		// Dependencies.
+		Logger: nil,
+
+		// Settings.
+		Description:    "",
+		GitCommit:      "",
+		Name:           "",
+		ProjectVersion: "",
+		Source:         "",
+	}
+}
+
+// New creates a new configured version service.
+func New(config Config) (*Service, error) {
+	// Dependencies.
+	if config.Logger == nil {
+		return nil, maskAnyf(invalidConfigError, "logger must not be empty")
+	}
+
+	// Settings.
+	if config.Description == "" {
+		return nil, maskAnyf(invalidConfigError, "description commit must not be empty")
+	}
+	if config.GitCommit == "" {
+		return nil, maskAnyf(invalidConfigError, "git commit must not be empty")
+	}
+	if config.Name == "" {
+		return nil, maskAnyf(invalidConfigError, "name must not be empty")
+	}
+	if config.ProjectVersion == "" {
+		return nil, maskAnyf(invalidConfigError, "project version must not be empty")
+	}
+	if config.Source == "" {
+		return nil, maskAnyf(invalidConfigError, "name must not be empty")
+	}
+
+	newService := &Service{
+		Config: config,
+
+		GoVersion: runtime.Version(),
+	}
+
+	return newService, nil
+}
+
+// Service implements the version service interface.
+type Service struct {
+	Config
+
+	GoVersion string
+}
+
+func (s *Service) Get(request Request) (*Response, error) {
+	s.Logger.Log("func", "Get")
+
+	response := DefaultResponse()
+
+	response.Description = s.Description
+	response.GitCommit = s.GitCommit
+	response.GoVersion = runtime.Version()
+	response.Name = s.Name
+	response.OSArch = runtime.GOOS + "/" + runtime.GOARCH
+	response.ProjectVersion = s.ProjectVersion
+	response.Source = s.Source
+
+	return response, nil
+}
